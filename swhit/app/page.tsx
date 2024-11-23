285235967b211cf24d1ac802fc4df0d9f8789b06
wheat-eco <wheat.sol.eco@gmail.com>
2024-11-23 18:13:35 +0100

    Update page.tsx

--- swhit/app/page.tsx
+++ swhit/app/page.tsx
@@ -2,6 +2,8 @@ import { NavBar } from "@/components/nav-bar"
 import { HeroSection } from "@/components/hero-section"
 import { FeaturesSection } from "@/components/features-section"
 import { RoadmapSection } from "@/components/roadmap-section"
+import { TokenomicsSection } from "@/components/tokenomics-section"
+import { AboutSection } from "@/components/about-section"
 import { Footer } from "@/components/footer"
 
 export default function Home() {
@@ -10,7 +12,9 @@ export default function Home() {
       <NavBar />
       <main>
         <HeroSection />
+        <AboutSection />
         <FeaturesSection />
+        <TokenomicsSection />
         <RoadmapSection />
       </main>
       <Footer />
