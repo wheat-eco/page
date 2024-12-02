fd65c5a60dc7e950b3670abdeb10c68301deafeb
WheatChain <wheat.sol.eco@gmail.com>
2024-12-02 21:11:24 +0100

    Update hero-section.tsx

--- swhit/components/hero-section.tsx
+++ swhit/components/hero-section.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { GithubIcon, Twitter, Send } from 'lucide-react'
 import Image from "next/image"
-import Link from "next/link"
+
 
 export function HeroSection() {
   return (
