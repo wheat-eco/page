ed319ab3623e5d6bce55c410a707cfa3e632f93f
WheatChain <wheat.sol.eco@gmail.com>
2024-12-02 20:35:47 +0100

    Update next.config.js

--- swhit/next.config.js
+++ swhit/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   images: {
-    domains: ['github.com', 'raw.githubusercontent.com'],
+    remotePatterns: ['github.com', 'raw.githubusercontent.com'],
   },
 }
 
