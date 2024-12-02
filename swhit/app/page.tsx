80895a719545507dba5a93ecb827e2da20535b05
WheatChain <wheat.sol.eco@gmail.com>
2024-12-02 21:48:13 +0100

    Update page.tsx

--- swhit/app/page.tsx
+++ swhit/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { NavBar } from "@/components/nav-bar"
 import { HeroSection } from "@/components/hero-section"
 import { FeaturesSection } from "@/components/features-section"
@@ -8,12 +7,6 @@ import { RoadmapSection } from "@/components/roadmap-section"
 import { TokenomicsSection } from "@/components/tokenomics-section"
 import { AboutSection } from "@/components/about-section"
 import { Footer } from "@/components/footer"
-
-const DynamicWhitepaper = dynamic(() => import('@/components/whitepaper').then(mod => mod.Whitepaper), {
-  ssr: false,
-  loading: () => <p>Loading whitepaper...</p>
-})
-
 
 export default function Home() {
   return (
@@ -25,9 +18,9 @@ export default function Home() {
         <FeaturesSection />
         <TokenomicsSection />
         <RoadmapSection />
-        <DynamicWhitepaper />
       </main>
       <Footer />
     </div>
   )
 }
+
