03ba9d8b7cdf636b92436df6759467165023b547
WheatChain <wheat.sol.eco@gmail.com>
2024-12-02 20:09:54 +0100

    Update page.tsx

--- swhit/app/page.tsx
+++ swhit/app/page.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import { NavBar } from "@/components/nav-bar"
 import { HeroSection } from "@/components/hero-section"
 import { FeaturesSection } from "@/components/features-section"
